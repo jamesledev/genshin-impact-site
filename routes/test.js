4a4f4a96f17b0864720936156527ca7c651652fc
jamesledev <74250657+jamesledev@users.noreply.github.com>
2020-12-16 18:43:27 +0000

    got the sheets api to work

--- routes/test.js
+++ routes/test.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios');
 
 /* GET home page. */
 router.get('/', async function (req, res, next) {
   try {
+    const url =
+      'https://sheets.googleapis.com/v4/spreadsheets/1Zh88v2hhi6rtkuinKOM2wm3po2WgHEMjHW_96R7Chp8/values/Build%20Options?key=AIzaSyAAuybQ5ZyL2J8VYX0mNUQJcR5qtkUVbqY&majorDimension=COLUMNS';
+    const response = await axios.get(url);
+
+    console.log(response.data);
+
     res.render('index', { title: 'Express' });
   } catch (error) {
     const message =
