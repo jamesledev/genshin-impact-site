e2b178522fbf0a348d371c22ab0a8395fae9b3a7
jamesledev <74250657+jamesledev@users.noreply.github.com>
2020-12-20 22:05:23 +0000

    Adding draggable

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const axios = require('axios');
+const fs = require('fs');
 
 function forLoop(response) {
   const characterDeet = {};
@@ -30,6 +31,21 @@ function forLoop(response) {
   }
   return { characterDeet, characterNames };
 }
+function imgNameGetter(directory) {
+  const files = fs.readdirSync(directory);
+  const artifactImages = [];
+  files.forEach((file) => {
+    const artifactType = file.split('-')[2].replace('.png', '');
+    const artifactSet = file.split('-')[0];
+    artifactImages.push({
+      file,
+      artifactType,
+      artifactSet,
+    });
+  });
+  return artifactImages;
+}
+
 /* GET home page. */
 router.get('/', async function (req, res, next) {
   try {
@@ -37,18 +53,21 @@ router.get('/', async function (req, res, next) {
       'https://sheets.googleapis.com/v4/spreadsheets/1Zh88v2hhi6rtkuinKOM2wm3po2WgHEMjHW_96R7Chp8/values/Build%20Options?key=AIzaSyAAuybQ5ZyL2J8VYX0mNUQJcR5qtkUVbqY&majorDimension=ROWS';
     const response = await axios.get(url);
     const { characterDeet, characterNames } = forLoop(response);
+    const imgNames = imgNameGetter('./public/images');
     const genshinObject = {
       characterDeet,
       characterNames,
       chracterDeetString: JSON.stringify(characterDeet),
       chracterNamestString: JSON.stringify(characterNames),
+      imgNames,
     };
-    console.log();
+    // console.log(genshinObject);
+
     res.render('index', genshinObject);
   } catch (error) {
     const message =
       error.response && error.response.status === 404
-        ? 'Some is wrong here...'
+        ? 'Something is wrong here...'
         : 'Generic Error';
     res.render('error', {
       message,
